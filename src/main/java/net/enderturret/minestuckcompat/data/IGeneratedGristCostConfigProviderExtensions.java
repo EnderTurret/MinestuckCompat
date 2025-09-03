1fab2d6be4ffcf2bf689a9d7698fafd40703c989
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2025-09-03 02:17:30 -0400

    Allow modifying the grist cost config before it's written.

---
+++ src/main/java/net/enderturret/minestuckcompat/data/IGeneratedGristCostConfigProviderExtensions.java
@@ -0,0 +1,10 @@
+package net.enderturret.minestuckcompat.data;
+
+import com.google.gson.JsonElement;
+
+public interface IGeneratedGristCostConfigProviderExtensions {
+
+	public default JsonElement modify(JsonElement root) {
+		return root;
+	}
+}
---
+++ src/main/java/net/enderturret/minestuckcompat/mixin/data/MixinGeneratedGristCostConfigProvider.java
@@ -0,0 +1,19 @@
+package net.enderturret.minestuckcompat.mixin.data;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import com.google.gson.JsonElement;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.mraof.minestuck.data.GeneratedGristCostConfigProvider;
+
+import net.enderturret.minestuckcompat.data.IGeneratedGristCostConfigProviderExtensions;
+
+@Mixin(GeneratedGristCostConfigProvider.class)
+public abstract class MixinGeneratedGristCostConfigProvider implements IGeneratedGristCostConfigProviderExtensions {
+
+	@ModifyExpressionValue(at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/DataResult;getOrThrow()Ljava/lang/Object;"), method = "run")
+	private Object minestuckcompat$allowModifications(Object elem) {
+		return modify((JsonElement) elem);
+	}
+}
