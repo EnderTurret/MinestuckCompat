f25397cc6f305865a617574e045ef1b6b33d6014
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2026-02-16 17:13:41 -0500

    Add `item_exists` conditions to all Quark compat recipes.
    I don't remember if Quark is one of those mods that actually disables
    items or just hides them, but this should avoid recipe errors if the
    items are actually disabled.

--- src/main/java/net/enderturret/minestuckcompat/data/recipe/QuarkRecipes.java
+++ src/main/java/net/enderturret/minestuckcompat/data/recipe/QuarkRecipes.java
@@ -36,24 +36,24 @@ public final class QuarkRecipes extends AbstractRecipeProvider {
 
 		// Corundum
 
-		gristCost(q("red_corundum")).grist(GARNET, 4).grist(AMETHYST, 12).build(output);
-		gristCost(q("red_corundum_cluster")).grist(GARNET, 3).grist(AMETHYST, 9).build(output);
-		gristCost(q("orange_corundum")).grist(IODINE, 4).grist(AMETHYST, 12).build(output);
-		gristCost(q("orange_corundum_cluster")).grist(IODINE, 3).grist(AMETHYST, 9).build(output);
-		gristCost(q("yellow_corundum")).grist(SULFUR, 4).grist(AMETHYST, 12).build(output);
-		gristCost(q("yellow_corundum_cluster")).grist(SULFUR, 3).grist(AMETHYST, 9).build(output);
-		gristCost(q("green_corundum")).grist(URANIUM, 4).grist(AMETHYST, 12).build(output);
-		gristCost(q("green_corundum_cluster")).grist(URANIUM, 3).grist(AMETHYST, 9).build(output);
-		gristCost(q("blue_corundum")).grist(BUILD, 4).grist(AMETHYST, 12).build(output);
-		gristCost(q("blue_corundum_cluster")).grist(BUILD, 3).grist(AMETHYST, 9).build(output);
-		gristCost(q("indigo_corundum")).grist(COBALT, 4).grist(AMETHYST, 12).build(output);
-		gristCost(q("indigo_corundum_cluster")).grist(COBALT, 3).grist(AMETHYST, 9).build(output);
-		gristCost(q("violet_corundum")).grist(MARBLE, 4).grist(AMETHYST, 12).build(output);
-		gristCost(q("violet_corundum_cluster")).grist(MARBLE, 3).grist(AMETHYST, 9).build(output);
-		gristCost(q("white_corundum")).grist(CHALK, 4).grist(AMETHYST, 12).build(output);
-		gristCost(q("white_corundum_cluster")).grist(CHALK, 3).grist(AMETHYST, 9).build(output);
-		gristCost(q("black_corundum")).grist(TAR, 4).grist(AMETHYST, 12).build(output);
-		gristCost(q("black_corundum_cluster")).grist(TAR, 3).grist(AMETHYST, 9).build(output);
+		gristCost(q("red_corundum")).grist(GARNET, 4).grist(AMETHYST, 12).build(output.withConditions(itemExists("quark", "red_corundum")));
+		gristCost(q("red_corundum_cluster")).grist(GARNET, 3).grist(AMETHYST, 9).build(output.withConditions(itemExists("quark", "red_corundum")));
+		gristCost(q("orange_corundum")).grist(IODINE, 4).grist(AMETHYST, 12).build(output.withConditions(itemExists("quark", "red_corundum")));
+		gristCost(q("orange_corundum_cluster")).grist(IODINE, 3).grist(AMETHYST, 9).build(output.withConditions(itemExists("quark", "red_corundum")));
+		gristCost(q("yellow_corundum")).grist(SULFUR, 4).grist(AMETHYST, 12).build(output.withConditions(itemExists("quark", "red_corundum")));
+		gristCost(q("yellow_corundum_cluster")).grist(SULFUR, 3).grist(AMETHYST, 9).build(output.withConditions(itemExists("quark", "red_corundum")));
+		gristCost(q("green_corundum")).grist(URANIUM, 4).grist(AMETHYST, 12).build(output.withConditions(itemExists("quark", "red_corundum")));
+		gristCost(q("green_corundum_cluster")).grist(URANIUM, 3).grist(AMETHYST, 9).build(output.withConditions(itemExists("quark", "red_corundum")));
+		gristCost(q("blue_corundum")).grist(BUILD, 4).grist(AMETHYST, 12).build(output.withConditions(itemExists("quark", "red_corundum")));
+		gristCost(q("blue_corundum_cluster")).grist(BUILD, 3).grist(AMETHYST, 9).build(output.withConditions(itemExists("quark", "red_corundum")));
+		gristCost(q("indigo_corundum")).grist(COBALT, 4).grist(AMETHYST, 12).build(output.withConditions(itemExists("quark", "red_corundum")));
+		gristCost(q("indigo_corundum_cluster")).grist(COBALT, 3).grist(AMETHYST, 9).build(output.withConditions(itemExists("quark", "red_corundum")));
+		gristCost(q("violet_corundum")).grist(MARBLE, 4).grist(AMETHYST, 12).build(output.withConditions(itemExists("quark", "red_corundum")));
+		gristCost(q("violet_corundum_cluster")).grist(MARBLE, 3).grist(AMETHYST, 9).build(output.withConditions(itemExists("quark", "red_corundum")));
+		gristCost(q("white_corundum")).grist(CHALK, 4).grist(AMETHYST, 12).build(output.withConditions(itemExists("quark", "red_corundum")));
+		gristCost(q("white_corundum_cluster")).grist(CHALK, 3).grist(AMETHYST, 9).build(output.withConditions(itemExists("quark", "red_corundum")));
+		gristCost(q("black_corundum")).grist(TAR, 4).grist(AMETHYST, 12).build(output.withConditions(itemExists("quark", "red_corundum")));
+		gristCost(q("black_corundum_cluster")).grist(TAR, 3).grist(AMETHYST, 9).build(output.withConditions(itemExists("quark", "red_corundum")));
 
 		// Glass shards
 		// So we have two options for these:
@@ -62,28 +62,28 @@ public final class QuarkRecipes extends AbstractRecipeProvider {
 		// Neither of these are great options. Either you can easily duplicate grist (block->shard) or you can easily obtain colored glass blocks (shard->block).
 		// Let's go with option b) as the lesser evil.
 
-		gristCost(tag("quark", "shards")).grist(BUILD, 1).build(output);
+		gristCost(tag("quark", "shards")).grist(BUILD, 1).build(output.withConditions(itemExists("quark", "white_shard")));
 
-		gristCost(q("dirty_glass")).grist(BUILD, 1).grist(RUST, 1).build(output);
+		gristCost(q("dirty_glass")).grist(BUILD, 1).grist(RUST, 1).build(output.withConditions(itemExists("quark", "dirty_glass")));
 
 		// Stone types
 
-		gristCost(q("jasper")).grist(BUILD, 2).grist(RUST, 1).build(output);
-		gristCost(q("limestone")).grist(BUILD, 2).grist(CHALK, 1).build(output);
-		gristCost(q("shale")).grist(BUILD, 2).grist(SHALE, 1).build(output);
-		gristCost(q("permafrost")).grist(BUILD, 2).grist(COBALT, 2).build(output); // Is permafrost even a stone?
+		gristCost(q("jasper")).grist(BUILD, 2).grist(RUST, 1).build(output.withConditions(itemExists("quark", "jasper")));
+		gristCost(q("limestone")).grist(BUILD, 2).grist(CHALK, 1).build(output.withConditions(itemExists("quark", "limestone")));
+		gristCost(q("shale")).grist(BUILD, 2).grist(SHALE, 1).build(output.withConditions(itemExists("quark", "shale")));
+		gristCost(q("permafrost")).grist(BUILD, 2).grist(COBALT, 2).build(output.withConditions(itemExists("quark", "permafrost"))); // Is permafrost even a stone?
 
-		gristCost(q("myalite")).grist(BUILD, 4).grist(SHALE, 3).build(output); // Based on the cost for End Stone.
-		gristCost(q("dusky_myalite")).grist(BUILD, 4).grist(SHALE, 3).grist(TAR, 2).build(output);
-		gristCost(q("myalite_crystal")).grist(SHALE, 3).grist(AMETHYST, 9).build(output); // Based on the cost for Amethyst Blocks.
+		gristCost(q("myalite")).grist(BUILD, 4).grist(SHALE, 3).build(output.withConditions(itemExists("quark", "myalite"))); // Based on the cost for End Stone.
+		gristCost(q("dusky_myalite")).grist(BUILD, 4).grist(SHALE, 3).grist(TAR, 2).build(output.withConditions(itemExists("quark", "dusky_myalite")));
+		gristCost(q("myalite_crystal")).grist(SHALE, 3).grist(AMETHYST, 9).build(output.withConditions(itemExists("quark", "myalite_crystal"))); // Based on the cost for Amethyst Blocks.
 
 		// Parrot eggs
 
-		gristCost(q("egg_parrot_blue")).grist(AMBER, 5).build(output);
-		gristCost(q("egg_parrot_gray")).grist(AMBER, 5).build(output);
-		gristCost(q("egg_parrot_green")).grist(AMBER, 5).build(output);
-		gristCost(q("egg_parrot_red_blue")).grist(AMBER, 5).build(output);
-		gristCost(q("egg_parrot_yellow_blue")).grist(AMBER, 5).build(output);
+		gristCost(q("egg_parrot_blue")).grist(AMBER, 5).build(output.withConditions(itemExists("quark", "egg_parrot_blue")));
+		gristCost(q("egg_parrot_gray")).grist(AMBER, 5).build(output.withConditions(itemExists("quark", "egg_parrot_gray")));
+		gristCost(q("egg_parrot_green")).grist(AMBER, 5).build(output.withConditions(itemExists("quark", "egg_parrot_green")));
+		gristCost(q("egg_parrot_red_blue")).grist(AMBER, 5).build(output.withConditions(itemExists("quark", "egg_parrot_red_blue")));
+		gristCost(q("egg_parrot_yellow_blue")).grist(AMBER, 5).build(output.withConditions(itemExists("quark", "egg_parrot_yellow_blue")));
 	}
 
 	private static Item q(String id) {
